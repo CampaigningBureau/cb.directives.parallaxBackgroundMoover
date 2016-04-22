030e23a9ec47aa42867abe751944dd647ced55da
Michael Scharl <michael.scharl@me.com>
2016-04-22 11:39:38 +0200

    add scroll handler

--- src/cb.directives.parallaxbackgroundmoover.js
+++ src/cb.directives.parallaxbackgroundmoover.js
@@ -8,11 +8,19 @@ ngModule.directive('parallaxBackgroundMoover', ["$window", function($window) {
     /** @property {string} limit directive to attribute or class definition */
     _directive.restrict = 'AC';
 
-    _directive.compile = function($element, $attrs) {
+    _directive.link = function($scope, $element, $attrs) {
+        function windowDidScroll() {
+            console.log('window');
+        }
 
+
+        $window.on('scroll', windowDidScroll);
+        $scope.$on('$destroy', function() {
+            $window.off('scroll', windowDidScroll);
+        });
     };
 
     return _directive;
 }]);
 
-module.exports = ngModule;
+module.exports = ngModuleName;
